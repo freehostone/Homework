14a4a870bade1cb338dc5ed5cc2c153140e0d306
freehostone <matttt2727@icloud.com>
2024-04-05 18:18:56 +0300

    one

---
+++ Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float p;
+            int age;
+            float percent;
+            Console.WriteLine("введите количество денег");
+            p=float.Parse(Console.ReadLine());
+            Console.WriteLine("введите количество лет");
+            age=int.Parse(Console.ReadLine());
+            Console.WriteLine("процент");
+            percent=float.Parse(Console.ReadLine());    
+            for(int i = 0; i < age; i++) 
+            {
+                p+=(p/100)*percent;
+                Console.WriteLine("на вашем счете "+p );
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 30;
+            while (age < 40) 
+            {
+                Console.WriteLine($"С днем рождения! Вам {age} лет");
+                age++;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
