8e4fc328149973154ed5f2b489cacaa53d817a13
freehostone <matttt2727@icloud.com>
2024-04-12 17:17:53 +0300

    Таблица

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 9;
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+               
+            };
+            Console.ReadLine();
+        }
+      
+            
+    }
+}
