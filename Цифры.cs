07323c897b48e65c3df0aa8111790b9333c3dd38
freehostone <matttt2727@icloud.com>
2024-04-25 15:31:44 +0300

    Цифры

---
+++ Цифры.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3};
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                Console.WriteLine(numbers[i]);
+              
+            }
+            Console.ReadLine();
+        }
+       
+    }
