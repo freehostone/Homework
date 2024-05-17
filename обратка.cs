f5d8de883201d7a75d5f9a1453f965aa7dcef800
freehostone <matttt2727@icloud.com>
2024-05-17 17:29:29 +0300

    обратка

---
+++ обратка.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int sum = 0;
+         foreach (var number in numbers)
+            {
+             if (number % 3 == 0)
+                {
+                  sum += number;
+                }
+            }
+        }
+    }
+    Console.Readline;
+}
+
+
