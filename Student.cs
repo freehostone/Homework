97c9bd3468b1d8113ac82d255e99694ca57e2e25
freehostone <matttt2727@icloud.com>
2024-06-07 13:08:55 +0300

    Student

---
+++ Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public double GradePointAverage { get; set; }
+
+            public Student(string name, int age, double gradePointAverage)
+            {
+                Name = name;
+                Age = age;
+                GradePointAverage = gradePointAverage;
+            }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Имя: {Name}");
+                Console.WriteLine($"Возраст: {Age}");
+                Console.WriteLine($"Средний балл: {GradePointAverage}");
+                if (GradePointAverage >= 4.5)
+                {
+                    Console.WriteLine("Отличник");
+                }
+                else if (GradePointAverage >= 3.5)
+                {
+                    Console.WriteLine("Хорошист");
+                }
+                else if (GradePointAverage >= 2.5)
+                {
+                    Console.WriteLine("Троечник");
+                }
+                else
+                {
+                    Console.WriteLine("Неуд");
+                }
+                Console.ReadLine();
+            }
+        }
+
+
+       
+        }
+    }
+}
